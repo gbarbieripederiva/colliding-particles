a75dbcd873f638ac14e940b5152ac5c798dc283d
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-04-14 04:00:05 -0300

    basic particle drawing

---
+++ visualization/particle_system.py
@@ -0,0 +1,31 @@
+class Particle:
+    def __init__( self, x = 0, y = 0,dx = 0, dy = 0,  m = 0, r = 0):
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.r = r
+        self.m = m
+    def __repr__(self):
+        return "{{x:{}, y:{}, dx:{}, dy:{}, m:{}, r:{}}}".format(
+            self.x , self.y , self.dx , self.dy , self.m , self.r 
+            )
+
+class ParticleCollideEvent:
+    def __init__(self, particles = [], time = 0):
+        self.particles = particles
+        self.time = time
+
+class SimulationData:
+    def __init__(self):
+        self.num_particles = 0
+        self.simulation_side_x = 0
+        self.simulation_side_y = 0
+        self.events = []
+
+    def __repr__(self):
+        return "{{ num_particles:{}, simulation_side_x:{}, simulation_side_y:{} }}".format(
+            self.num_particles,
+            self.simulation_side_x,
+            self.simulation_side_y
+        )
---
+++ visualization/visualizer.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import data_parser as data_parser
+import matplotlib.collections
+import sys
+import argparse
+
+argp = argparse.ArgumentParser(description="Particle neighbour visualizer")
+argp.add_argument('--particle-file-path', dest='particle_path', help='particle file path')
+argp.add_argument('--output-file-path', dest='output_path', help='output file path')
+argp.add_argument('pid',nargs='?', help='particle id to highlight')
+
+args = argp.parse_args()
+
+sim_data = data_parser.data_getter(
+    particle_file_path=args.particle_path,
+    output_file_path=args.output_path
+    )
+
+def show_particles():
+    patches = [plt.Circle((p.x,p.y), p.r) for p  in  sim_data.events[0].particles]
+    ax = plt.gca()
+    ax.clear()
+    ax.figure.set_size_inches(14,10)
+    coll = matplotlib.collections.PatchCollection(patches, facecolors="black")
+    ax.add_collection(coll)
+    ax.margins(0.01)
+    ax.figure.canvas.draw()
+        
+show_particles()
+plt.show()
